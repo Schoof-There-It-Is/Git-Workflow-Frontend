b15a522f14914c420f443e151e94751202fef84f
philschoof <phil.schoof@gmail.com>
2016-05-05 10:57:41 -0400

    Add AJAX get request to api.js

---
+++ assets/scripts/api.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let getPeople = function(success, failure){
+  $.ajax({
+    url: 'http://jsonplaceholder.typicode.com/users',
+  }).done(success)
+  .fail(failure);
+};
+
+module.exports = getPeople;
